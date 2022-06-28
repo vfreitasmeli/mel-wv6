8a04429c153f313e4a3a7e3304f4ba9657b7b177
EmersonPaduan <emerson.paduan@gmail.com>
2022-06-28 16:16:17 -0300

    Aplicação ArrayList and HashMap

--- exemploInterface/src/AppConta.java
+++ exemploInterface/src/AppConta.java
@@ -1,7 +1,11 @@
+import modelo.Conta;
 import modelo.ContaCorrente;
 import modelo.ContaEspecial;
 import util.MongoDB;
 import util.MySQL;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AppConta {
 
@@ -12,8 +16,31 @@ public class AppConta {
         MySQL mySQL = new MySQL();
         MongoDB mongoDB = new MongoDB();
 
-        cc1.gravar(mySQL);
+        ArrayList<Conta> listaDeContas = new ArrayList();
+        HashMap<Integer, Conta> mapadeContas = new HashMap<>();
 
-        ce1.gravar(mongoDB);
+        listaDeContas.add(cc1);
+        listaDeContas.add(ce1);
+
+//        for (int i = 0; i < contas.size() ; i++) {
+//            System.out.println("Conta: " + contas.get(i).getNumero());
+//        }
+
+
+
+//        cc1.gravar(mySQL);
+//        ce1.gravar(mongoDB);
+
+        mapadeContas.put(cc1.getNumero(), cc1);
+        mapadeContas.put(ce1.getNumero(), ce1);
+
+        for (Conta c : listaDeContas) {  //foreach : para cada Conta da estrutura listaDeContas
+            if(c.getNumero() == 111) {
+                System.out.println("Conta Encontrada no Array: " + c.getNumero());
+            }
+        }
+
+        Conta contaEncontrada = mapadeContas.get(111);
+        System.out.println("Conta Encontrada no HashMap: " + contaEncontrada.getNumero());
     }
 }
---
+++ exemploInterface/src/exemplos/Exemplo.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        int numeros[] = new int[10];
+
+
+    }
+}
+
+class Vetor{
+    private double numeros[] = new double[10];
+
+    public void adicionar(double n) {
+
+    }
+}
